5e59a340de12ac93a9d52d60667cb363cac79132
chengjo0 <cheng.john@pm.me>
2019-07-25 23:54:50 +0300

    back to doIt

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,7 +8,6 @@ git checkout origin/master;
 function doIt() {
 	rsync --exclude ".git/" \
 		--exclude ".DS_Store" \
-		--exclude ".osx" \
 		--exclude "bootstrap.sh" \
 		--exclude "README.md" \
 		--exclude "LICENSE-MIT.txt" \
@@ -17,14 +16,12 @@ function doIt() {
 }
 
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
-	# doIt;
-  echo "forced me..."
+	doIt;
 else
 	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
 	echo "";
 	if [[ $REPLY =~ ^[Yy]$ ]]; then
-		# doIt;
-    echo "yay..."
+		doIt;
 	fi;
 fi;
 unset doIt;
