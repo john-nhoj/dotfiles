7b3b787c8ccb7c23a2aa0c0f6793622d569447c2
Private <cheng.john@pm.me>
2021-06-08 15:19:43 +0300

    Fix linter errors

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,9 +7,9 @@ NOCOLOR="\033[0m"
 echo -e "${RED}==> Linking root configurations${NOCOLOR}"
 
 function doIt() {
-	for file in `ls -A | egrep -vi '.sh|.git|README|.macos|com.googlecode.iterm2.plist|tmp'`
+	for file in $(ls -A | grep -E -vi '.sh|.git|README|.macos|com.googlecode.iterm2.plist|tmp')
   do
-    ln -s ${BASEDIR}/${file} ~/${file}
+    ln -s "${BASEDIR}"/"${file}" ~/"${file}"
   done
   for app in "iTerm2" \
     "Terminal"; do
@@ -20,7 +20,7 @@ function doIt() {
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
 	doIt;
 else
-	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
+	read -rp "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
 	echo "";
 	if [[ $REPLY =~ ^[Yy]$ ]]; then
 		doIt;
