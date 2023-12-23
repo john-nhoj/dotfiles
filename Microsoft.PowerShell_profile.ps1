3fe02870eba6990010139f7b8be6b51e35fd78b5
John Cheng <cheng.john@pm.me>
2023-12-23 10:56:33 +0800

    feat: add pwsh profile

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,2 @@
+oh-my-posh init pwsh | Invoke-Expression
+oh-my-posh init pwsh --config 'https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/main/themes/onehalf.minimal.omp.json' | Invoke-Expression
