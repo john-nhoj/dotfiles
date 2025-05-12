9178ba471a1bd625b48f980e39d7474347386148
John Cheng (john-nhoj) <cheng.john@pm.me>
2025-05-12 23:23:03 +0100

    feat: no clue what changed

--- config/nvim/lua/plugins/mini.lua
+++ config/nvim/lua/plugins/mini.lua
@@ -1,10 +1,11 @@
 return {
 	{ "echasnovski/mini.nvim", version = false },
-	{ 
-		"echasnovski/mini.comment", 
+	{
+		"echasnovski/mini.comment",
 		version = false,
 		config = function()
 			require("mini.comment").setup()
 		end
 	}
 }
+
