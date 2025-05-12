471d7c5c0449c3145eda69cf5b54f36f4b59b2e3
John Cheng (john-nhoj) <cheng.john@pm.me>
2025-05-12 23:54:59 +0100

    feat: tailor explorer experience

--- config/nvim/lua/plugins/snacks.lua
+++ config/nvim/lua/plugins/snacks.lua
@@ -8,9 +8,18 @@ return {
 			bigfile = { enabled = true },
 			dashboard = {
 				enabled = true,
-				example = "compact_files",
+				sections = {
+          { section = "header" },
+          { icon = " ", title = "Keymaps", section = "keys", indent = 2, padding = 1 },
+          { icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
+          { icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
+          { section = "startup" },
+        },
 			},
-			explorer = { enabled = true },
+			explorer = {
+        enabled = true,
+        replace_netrw = false,
+      },
 			indent = {
 				enabled = true,
 				only_scope = true,
