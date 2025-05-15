1affd73395053ca94fa37d76845f53b0b0200924
John Cheng (john-nhoj) <cheng.john@pm.me>
2025-05-15 22:56:51 +0100

    feat: remove mini file explorer

--- config/nvim/lua/plugins/mini.lua
+++ config/nvim/lua/plugins/mini.lua
@@ -7,25 +7,5 @@ return {
 			require("mini.comment").setup()
 		end
 	},
-  {
-    "echasnovski/mini.files",
-    version = "*",
-    config = function ()
-      local MiniFiles = require("mini.files")
-      MiniFiles.setup({
-        mappings = {
-          go_in = "<CR>", -- Map both Enter and L to enter directories or open files
-          go_in_plus = "L",
-          go_out = "-",
-          go_out_plus = "H",
-        },
-        vim.keymap.set("n", "<leader>ee", "<cmd>lua MiniFiles.open()<CR>", { desc = "Toggle mini file explorer" }), -- toggle file explorer
-        vim.keymap.set("n", "<leader>ef", function()
-          MiniFiles.open(vim.api.nvim_buf_get_name(0), false)
-          MiniFiles.reveal_cwd()
-        end, { desc = "Toggle into currently opened file" })
-      })
-    end
-  }
 }
 
