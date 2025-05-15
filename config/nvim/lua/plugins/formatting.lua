a01a46e6d420a67777d4fef683b1d07c664d1b97
John Cheng (john-nhoj) <cheng.john@pm.me>
2025-05-15 22:56:30 +0100

    feat: add formatters

---
+++ config/nvim/lua/plugins/formatting.lua
@@ -0,0 +1,20 @@
+return {
+  "stevearc/conform.nvim",
+  config = function()
+    require("conform").setup({
+      formatters_by_ft = {
+        lua = { "stylua" },
+        -- Conform will run multiple formatters sequentially
+        python = { "isort", "ruff" },
+        -- You can customize some of the format options for the filetype (:help conform.format)
+        rust = { "rustfmt", lsp_format = "fallback" },
+        -- Conform will run the first available formatter
+        javascript = { "prettierd", "prettier", stop_after_first = true },
+      },
+      format_on_save = {
+        timeout_ms = 500,
+        lsp_format = "fallback",
+      },
+    })
+  end
+}
