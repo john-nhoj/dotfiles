8f9adc632a840c74945fbbdc4ea54ca85cf174d6
John Cheng (john-nhoj) <cheng.john@pm.me>
2025-05-22 20:10:50 +0100

    feat: add tab keybinds

--- config/nvim/lua/config/keybinds.lua
+++ config/nvim/lua/config/keybinds.lua
@@ -9,38 +9,40 @@ vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "Moves lines up in visual 
 vim.keymap.set("n", "n", "nzzzv", { desc = "Navigate to next search occurence and keep it centered" })
 vim.keymap.set("n", "N", "Nzzzv", { desc = "Navigate to previous search occurence and keep it centered" })
 
-vim.keymap.set("v", ">", ">gv", opts, { desc = "Increase indent without exiting the selected content" })
-vim.keymap.set("v", "<", "<gv", opts, { desc = "Decrease indent without exiting the selected content" })
+vim.keymap.set("v", ">", ">gv", { desc = "Increase indent without exiting the selected content" })
+vim.keymap.set("v", "<", "<gv", { desc = "Decrease indent without exiting the selected content" })
 
 vim.keymap.set("x", "<leader>p", [["_dP]], { desc = "Paste clipboard without replacing clipboard content" })
-vim.keymap.set("v", "p", '"_dp', opts) -- Not sure what it does
 
 vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]], { desc = "Delete without copying the content" })
 
 vim.keymap.set("n", "<C-c>", ":nohl<CR>", { desc = "Clear search highlights", silent = true })
 
--- vim.keymap.set("n", "<leader>f", vim.lsp.buf.format, { desc = "Run formatter" })
-
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]], {
-	desc = "Replace word cursor is on globally",
+  desc = "Replace word cursor is on globally",
 })
 
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", {
-	desc = "Make file executable",
-	silent = true,
+  desc = "Make file executable",
+  silent = true,
 })
+
+-- tab stuff
+vim.keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "open new tab" })
+vim.keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "close current tab" })
+vim.keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" })
+vim.keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" })
+vim.keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current tab in a new tab" })
 
 vim.keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" })
 vim.keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" })
 vim.keymap.set("n", "<leader>se", "<C-w>=", { desc = "Resize panes equally" })
 vim.keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current pane" })
--- TODO: missing some navigation between panes
 
 vim.keymap.set("n", "<leader>fp", function()
-	-- Get the file path relative to the home directory
-	local file_path = vim.fn.expand("%:~")
-	-- Copy the file path to the clipboard register
-	vim.fn.setreg("+", file_path)
-	print("File path copied to clipboard: " .. file_path)
+  -- Get the file path relative to the home directory
+  local file_path = vim.fn.expand("%:~")
+  -- Copy the file path to the clipboard register
+  vim.fn.setreg("+", file_path)
+  print("File path copied to clipboard: " .. file_path)
 end, { desc = "Copy file path to clipboard" })
-
